a4f8acad4c0ba2054421602d48c85032f0bdcc5f
Mike <mike.rayko@gmail.com>
2021-09-08 15:15:21 +0300

    upd conda install

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-#conda create -n viralflye -c bioconda -c conda-forge  "python>=3.6" prodigal viralverify samtools seqtk minced minimap2
-source /home/mike/Libs/miniconda3/etc/profile.d/conda.sh
+conda create -n viralflye -c bioconda -c conda-forge  "python>=3.6" prodigal viralverify samtools seqtk minced minimap2
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
 conda activate viralflye
-#pip install biopython scipy pysam
+pip install biopython scipy pysam
 git clone  https://github.com/Dmitry-Antipov/viralFlye
 git clone  https://github.com/ablab/viralComplete
 export PATH=$PATH:$(pwd)/viralFlye

f08c9801e580b28386ffedaf71f1fe9db1c1fa8a
Dmitry Antipov <dmitrij.antipov@gmail.com>
2021-09-27 14:57:09 +0300

    minor

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-conda create -n viralflye -c bioconda -c conda-forge  "python>=3.6" prodigal viralverify samtools seqtk minced minimap2 biopython pysam tabix samtools freebayes bcftools numpy
+conda create -n viralFlye -c bioconda -c conda-forge  "python>=3.6" prodigal viralverify samtools seqtk minced minimap2 biopython pysam tabix samtools freebayes bcftools numpy
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 conda activate viralflye
-git clone  https://github.com/Dmitry-Antipov/viralFlye
+#git clone  https://github.com/Dmitry-Antipov/viralFlye
 git clone  https://github.com/ablab/viralComplete
-export PATH=$PATH:$(pwd)/viralFlye
+#export PATH=$PATH:$(pwd)/viralFlye
+export PATH=$PATH:$(pwd)
 export PATH=$PATH:$(pwd)/viralComplete
-chmod +x viralFlye/viralFlye.py
+chmod +x viralFlye.py
 chmod +x viralComplete/viralcomplete.py
-viralFlye/viralFlye.py
+viralFlye.py
